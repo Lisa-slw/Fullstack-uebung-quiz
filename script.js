8b4f7410a8d24902d881b54d1bcb60d4abcf14aa
lisa.sobolewski <lisa.sobolewski@accenture.com>
2026-09-16 16:56:50 +0200

    Rechenauoperationen/aufgaben

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+//
+//console.log("hello world");
+
+//let number = 20;
+//console.log (number);
+
+//let zahl1 = 5;
+//let zahl2 = 4;  
+//console.log(zahl1+zahl2);
+//console.log(zahl1-zahl2);
+//console.log(zahl1*zahl2);
+
+//console.log(3 == '5');
+//console.log(5 <= 4 );
+
+function functionName(zahl1, zahl2){
+    let result = zahl1 + zahl2;
+    return result; 
+}
+console.log(functionName(5,5));
+
