167d7eb92031e546ba4202c0f53adfa0a41c6b05
lisa.sobolewski <lisa.sobolewski@accenture.com>
2026-09-17 11:04:09 +0200

    If-Schleifen

--- script.js
+++ script.js
@@ -13,9 +13,16 @@
 //console.log(3 == '5');
 //console.log(5 <= 4 );
 
-function functionName(zahl1, zahl2){
-    let result = zahl1 + zahl2;
-    return result; 
-}
-console.log(functionName(5,5));
+//function functionName(zahl1, zahl2){
+//    let result = zahl1 + zahl2;
+//    return result; 
+//}
+//console.log(functionName(5,5));
 
+let zahl = 5; 
+if (zahl ==5){
+    console.log("Die Zahl ist eine 5")
+}
+else {
+    console.log("Die Zahl ist nicht 5")
+}
