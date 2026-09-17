932a484ed1ccbd8a2028eb9f1cc8d30981ad2f17
lisa.sobolewski <lisa.sobolewski@accenture.com>
2026-09-17 14:16:43 +0200

    test

--- script.js
+++ script.js
@@ -19,10 +19,81 @@
 //}
 //console.log(functionName(5,5));
 
-let zahl = 5; 
-if (zahl ==5){
-    console.log("Die Zahl ist eine 5")
-}
-else {
-    console.log("Die Zahl ist nicht 5")
-}
+//let istDurstig = true; 
+//let willKaffee = true;
+
+//if (istDurstig){
+//    if (willKaffee){
+//         //Kaffe kochen;
+//   }
+//}
+
+
+//let number1=5;
+//let number2=6;
+//let result =0;
+
+//Größer-KLeiner Funktion
+//function compare (number1, number2){
+//    if (number1 > number2){
+//        return number1;
+//    } else {
+//        return number2;
+//    }
+//}
+//result = compare(number1, number2);
+//console.log(result);
+
+//Zahlen vergleichen (verkürzte Variante geht auch mit if)
+//function compare (number1, number2){
+//    return number1 == number2; 
+//}
+//console.log(compare(number1, number2));
+
+//(Un)grade Zahlen
+//function isEven (number1){
+//    if (number1% 2 == 0){
+//        return "Die Zahl ist gerade";
+//    } else{
+//        return "Die Zahl ist ungerade";
+//    }
+//}
+//console.log(isEven(number1));
+
+//switch / case 
+
+//let difficulty = 2; 
+
+//switch(difficulty){
+    //case 1: console.log("Easy");
+    //break;
+    //case 2: console.log("Medium");
+    //break; 
+    //case 3: console.log("Hard");
+    //break;
+//}
+
+//let canZombiesSmashDoors = (difficulty == 3) ? true : false;
+//console.log("Can Zombies smash Doors? " + canZombiesSmashDoors);
+
+//Schleife 
+//for-loop
+//for (let i = 0; i < 5; i++){
+    //console.log(i)
+//}
+
+//while loop
+//let zahl = 1;
+//while (zahl < 5){
+    //zahl = zahl +2;
+    //console.log(zahl);
+//}
+
+//arrays
+//creating an array
+//let birthdayVisitors = ["Anna", "Bastian", "Kevin", "Denise"];
+//Adding an element later 
+//birthdayVisitors.push("Elena");
+//let name = birthdayVisitors[1]; //return Batians name [1]-> Platz 2 Liste
+
+// Schleife mit Nummern 1-10
